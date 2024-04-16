887e7191c5676a6bfb7798793dca8cd16a850100
Kosuke Miyazaki <27inc.kosukemiyazaki@gmail.com>
2024-04-16 17:27:24 +0900

    Initial Commit

---
+++ SeconKadaiApp_3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeconKadaiApp_3
+//
+//  Created by Kosuke Miyazaki on 2024/04/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
